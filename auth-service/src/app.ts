839c8618c43063c9f8a2fa3e2249edd09cb9f207
Jesus Camacaro <jesus.alejandr@gmail.com>
2025-02-23 08:26:05 -0300

    feat(auth-service): server implement

--- auth-service/src/app.ts
+++ auth-service/src/app.ts
@@ -1,3 +1,10 @@
+import {AuthApplication} from "./auth/application/authApplication";
+import {RedisConnection} from "./auth/infra/outgoing/redis";
+import {Server} from "./auth/server/init";
+import {config} from "./config";
 
-const mensaje: string = 'Hola, TypeScript con Node.js!';
-console.log(mensaje);
+const redisRepository = new RedisConnection(config.redis.connection);
+const authApplication = new AuthApplication(redisRepository);
+
+const server = new Server(config, authApplication);
+server.listen()
