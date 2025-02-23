0b76cbe7f408af1587a993405329311cecfd1ec4
Jesus Camacaro <jesus.alejandr@gmail.com>
2025-02-23 08:01:01 -0300

    feat(auth-service): config added

---
+++ auth-service/src/config.ts
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export interface Config {
+    server: {
+        port: string;
+    };
+    jwt: {
+        secret: string;
+        expiresIn: string;
+    };
+    redis: {
+        connection: string;
+    };
+}
+
+export const config: Config = {
+    server: {
+        port : process.env.PORT as string,
+    },
+    jwt: {
+        secret: process.env.JWT_SECRET as string,
+        expiresIn: process.env.JWT_EXPIRES_IN as string,
+    },
+    redis: {
+        connection: process.env.REDIS_CONNECTION as string,
+    },
+}
